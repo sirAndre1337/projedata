ee51c4007975154ab1c209cebce93489413b4d19
André Luis <andrelluis17@hotmail.com>
2022-04-04 11:51:18 -0300

    project created

---
+++ backend/src/main/java/com/andre/projedata/ProjedataApplication.java
@@ -0,0 +1,13 @@
+package com.andre.projedata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjedataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjedataApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/andre/projedata/ProjedataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andre.projedata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjedataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
